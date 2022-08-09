4113580ec9843b63a9edc2939c72dbecd7b8c6c9
Aditya <adikpote@gmail.com>
2022-08-10 04:35:21 +0530

    minor changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send('succesfully connected!');
+	res.send('its working');
 });
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
