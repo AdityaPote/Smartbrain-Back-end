a9d8ab200edebfcf6f085068e86c7934904fdd87
Aditya <adikpote@gmail.com>
2023-03-30 22:46:57 +0530

    Added dotenv
    Signed-off-by: Aditya <adikpote@gmail.com>

--- server.js
+++ server.js
@@ -1,39 +1,50 @@
-const express = require ('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors');
-const knex = require('knex')
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+require("dotenv").config();
 
-const signin = require('./controllers/signin')
-const register = require('./controllers/register'); 
-const profile = require('./controllers/profile'); 
-const image = require('./controllers/image'); 
+const signin = require("./controllers/signin");
+const register = require("./controllers/register");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-	client: 'pg',
-	connection: {
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
-    		rejectUnauthorized: false
-  		}
-	}
-})
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send('its working');
+app.get("/", (req, res) => {
+  res.send("its working");
 });
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log(`App is running on port ${process.env.PORT}`)
+  console.log(`App is running on port ${process.env.PORT}`);
 });
